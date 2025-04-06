c1c3b70e47c208f078dde1f033410b1445f1f62e
shreyash <sk@macbook16.local>
2025-04-07 02:55:27 +0530

    first java commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,23 @@
  * This is a general purpose Gradle build.
  * Learn more about Gradle by exploring our Samples at https://docs.gradle.org/8.12.1/samples
  */
+plugins {
+    java
+}
+tasks.named<Jar>("jar") {
+  manifest {
+  attributes["Main-Class"] = "com.gradlehero.languageapp.SayHello"
+  }
+}
+repositories {
+  mavenCentral()
+}
+
+
+dependencies {
+  testImplementation("org.junit.jupiter:junit-jupiter:5.10.0")
+}
+
+tasks.named<Test>("test") {
+  useJUnitPlatform()
+}
